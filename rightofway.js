a74c1c8181bd9a70fedbed595b8f9ea6e762afee
Michael Zeng <mzeng7@berkeley.edu>
2018-11-04 19:38:23 -0800

    add function documentation; start an object oriented approach

--- rightofway.js
+++ rightofway.js
@@ -1,13 +1,51 @@
+/** Object containing array of Actions, representing the call made by the ref. */
+let Call = {
+  actions : [],
+  result : "none",
+  toString : function() {
+    let result = "";
+    let i;
+    for (i = 0; i < actions.length - 1; i++) {
+      result += actions[i].toString() + ", ";
+    }
+    result += actions[i].toString() + ".";
+    return result;
+  }
+};
+
+/** Object representing a single fencing action.
+ * @param {string} name - the name of the action, e.g. "attack", "parry",
+                          "riposte", etc.
+ * @param {string} fencer - either "left" or "right": the fencer that performed
+                            this action
+ * @param {string} result - optional (should be defined later, though).
+                            the result of the action, e.g. "arrives", "is no",
+                            "off target", etc.
+ */
+function Action(type, fencer, result) {
+  this.type = type;
+  this.fencer = fencer;
+  this.result = result || "none";
+}
+
+Action.prototype.toString = function() {
+  return this.name + " " this.fencer + " " + this.result;
+}
+
+/** Resets the buttons to the initial state. */
 function reset() {
   document.getElementById("userPrompt").style.visibility = "visible";
   document.getElementById("buttons").style.visibility = "visible";
   document.getElementById("priority").style.visibility = "visible"
   document.getElementById("result").style.visibility = "hidden";
   document.getElementById("result").innerHTML = "<strong>Referee calls:</strong> ";
+  Call.actions = [];
+  Call.result = "none";
   updatePriority("none");
   initial();
 }
 
+/** Creates the first buttons that the user will choose from. */
 function initial() {
   document.getElementById("userPrompt").innerHTML = "What was the action?";
   const actions = {
@@ -20,6 +58,9 @@ function initial() {
   createButtons(actions, initialUpdate);
 }
 
+/** Determines what happens after one of the initial buttons is chosen.
+ * @param {Object} event - contains attributes of the button that called this function
+ */
 function initialUpdate(event) {
   document.getElementById("result").style.visibility = "visible";
   if (event.target.id === "attackleft") {
@@ -46,45 +87,59 @@ function initialUpdate(event) {
   }
 }
 
-function attackResult(attack_type) {
-  document.getElementById("userPrompt").innerHTML = "What was the result of the " + attack_type + "?"
+/** Creates the buttons that will ask the user what happened after a kind of attack.
+ * @param {string} attackType - the kind of aggressive action, e.g. "attack",
+                                "counterattack", "riposte"
+ */
+function attackResult(attackType) {
+  Call.actions.push(new Action(attackType, currentPriority()));
+  document.getElementById("userPrompt").innerHTML = "What was the result of the " + attackType + "?";
   let b = document.getElementById("attackButtons");
   let actions;
-  if (attack_type === "attack") {
+  if (attackType === "attack") {
     actions = {
-      "arrives": attack_type + " arrives",
-       "offtarget": attack_type + " off target",
-       "misses": attack_type + " misses",
-       "parried": attack_type + " is parried"};
+      "arrives": attackType + " arrives",
+       "offtarget": attackType + " off target",
+       "misses": attackType + " misses",
+       "parried": attackType + " is parried"};
   } else {
      actions = {
-       "arrives": attack_type + " arrives",
-       "offtarget": attack_type + " off target",
-       "misses2nd": attack_type + " misses",
-       "counterparried": attack_type + " is counterparried"
+       "arrives": attackType + " arrives",
+       "offtarget": attackType + " off target",
+       "misses2nd": attackType + " misses",
+       "counterparried": attackType + " is counterparried"
      };
   }
   createButtons(actions, attackUpdate);
 }
 
+/** Determines what happens after one of the buttons from attackResult is chosen.
+ * @param {Object} event - contains attributes of the button that called this function
+ */
 function attackUpdate(event) {
   // document.getElementById("result").style.visibility = "visible";
+  let currentAction = Call.actions[Call.actions.length - 1];
   if (event.target.id === "arrives") {
     updateResult("arrives, ");
+    currentAction.result = "arrives";
     awardTouch();
   } else if (event.target.id === "offtarget") {
     updateResult("is off target, ");
+    currentAction.result = "off target";
     noTouch();
   } else if (event.target.id === "misses") {
     updateResult("is no, ");
+    currentAction.result = "is no";
     switchPriority();
     defenderResponse();
   } else if (event.target.id === "misses2nd") {
     updateResult("is no, ");
+    currentAction.result = "is no";
     switchPriority();
     attackContinuation();
   } else if (event.target.id === "parried") {
     updateResult("is parried, ");
+    currentAction.result = "parried";
     switchPriority();
     riposte();
   } else if (event.target.id === "counterparried") {
@@ -96,12 +151,16 @@ function attackUpdate(event) {
   }
 }
 
+/** Creates the buttons that will ask the user whether a riposte occurred after a parry. */
 function riposte() {
   document.getElementById("userPrompt").innerHTML = "Was there a riposte attempt after the parry?";
   const actions = {"y": "Yes, riposte", "n": "No riposte"};
   createButtons(actions, riposteUpdate);
 }
 
+/** Determines what happens after one of the buttons from riposte() is chosen.
+ * @param {Object} event - contains attributes of the button that called this function
+ */
 function riposteUpdate(event) {
   if (event.target.id === "y") {
     updateResult("riposte ");
@@ -113,12 +172,16 @@ function riposteUpdate(event) {
   }
 }
 
+/** Creates the buttons that ask the user whether a counterattack was performed by the defender. */
 function defenderResponse() {
   document.getElementById("userPrompt").innerHTML = "Did the fencer on the " + currentPriority() + " respond with a counterattack?"
   const response = {"y": "Counterattack", "n": "No counterattack"};
   createButtons(response, defenderUpdate);
 }
 
+/** Determines what happens after one of the buttons from defenderResponse is chosen.
+ * @param {Object} event - contains attributes of the button that called this function
+ */
 function defenderUpdate(event) {
   if (event.target.id === "y") {
     updateResult("counterattack ");
@@ -129,12 +192,19 @@ function defenderUpdate(event) {
   }
 }
 
+/** Creates the buttons that ask whether a remise/attack continuation was performed.
+ * NOTE: Currenly supports only remises. Maybe add reprises or redoublements in future,
+ * depending on demand.
+ */
 function attackContinuation() {
   document.getElementById("userPrompt").innerHTML = "Did the fencer on the " + currentPriority() + " continue their action?";
   const actions = {"remise": "Yes, remise", "no": "No"};
   createButtons(actions, continuationUpdate);
 }
 
+/** Determines what happens after one of the buttons from attackContinuation is chosen.
+ * @param {Object} event - contains attributes of the button that called this function
+ */
 function continuationUpdate(event) {
   if (event.target.id === "remise") {
     updateResult("remise ");
@@ -145,23 +215,27 @@ function continuationUpdate(event) {
   }
 }
 
+/** Ends the current fencing phrase with a touch to the fencer with priority. */
 function awardTouch() {
   updateResult("touch " + currentPriority() + ".");
+  Call.result = currentPriority();
   showResult();
 }
 
+/** Ends the current fencing phrase with no touch to either fencer. */
 function noTouch() {
   updateResult("no touch.");
   showResult();
 }
 
+/** Hides all buttons and user prompt messages,
+ * shows the final result, if it is not already visible. */
 function showResult() {
   document.getElementById("userPrompt").style.visibility = "hidden";
   document.getElementById("buttons").style.visibility = "hidden";
   document.getElementById("priority").style.visibility = "hidden";
   document.getElementById("result").style.visibility = "visible";
 }
-
 
  /**
  * Creates the buttons given a set of actions and results.
@@ -188,6 +262,8 @@ function createButtons(actions, resultFunc) {
   }
 }
 
+/** Creates the reset button that allows the user to cancel their input and
+  * return to the start. Called directly within index.html. */
 function createResetButtion() {
   let resetElement = document.getElementById("resetbutton");
   let resetButton = document.createElement("input");
@@ -198,6 +274,7 @@ function createResetButtion() {
   resetElement.appendChild(resetButton);
 }
 
+/** Sets the fencer that currently has the priority, based on user input. */
 function updatePriority(fencer) {
   if (fencer === "none") {
     document.getElementById("priority").innerHTML = "<strong>Current priority:</strong> None";
@@ -208,6 +285,7 @@ function updatePriority(fencer) {
   }
 }
 
+/** Switches the current priority from left to right or left to right. */
 function switchPriority() {
   if (currentPriority() === "right") {
     updatePriority("left");
@@ -218,6 +296,7 @@ function switchPriority() {
   }
 }
 
+/** Gets the current priority set by updatePriority and switchPriority */
 function currentPriority() {
   if (document.getElementById("priority").innerHTML === "<strong>Current priority:</strong> Right") {
     return "right";
@@ -230,6 +309,9 @@ function currentPriority() {
   }
 }
 
+/** Updates the result displayed in the "result" element.
+ * NOTE: Possibly could be superceded by the Call object's toString method.
+ */
 function updateResult(action) {
   document.getElementById("result").innerHTML += action;
 }
